8f075845d402b7d66a94d1e38488ae3434bb9694
Zack Garza (DevR) <dzackgarza@gmail.com>
2014-11-05 19:30:34 -0800

    vim update

--- aliases/bash_aliases.zsh
+++ aliases/bash_aliases.zsh
@@ -16,3 +16,16 @@ export JAVA_HOME='/usr/libexec/java_home'
 
 alias droplet='ssh root@104.131.9.12'
 alias berkeley='ssh -XC -c blowfish-cbc,arcfour cory.eecs.berkeley.edu -l cs70-awu'
+
+function build_ctags_php() {
+	ctags -f ~/.vim/tags/myphpproject \
+	--langmap="php:+.inc" -h ".php.inc" -R \
+	--exclude='*.js' \
+	--exclude='*.sql' \
+	--totals=yes \
+	--tag-relative=yes \
+	--PHP-kinds=+cf-v \
+	--regex-PHP='/abstract\s+class\s+([^ ]+)/\1/c/' \
+	--regex-PHP='/interface\s+([^ ]+)/\1/c/' \
+	--regex-PHP='/(public\s+|static\s+|abstract\s+|protected\s+|private\s+)function\s+\&?\s*([^ (]+)/\2/f/'
+}
