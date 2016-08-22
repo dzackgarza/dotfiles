e457acf3c8725b8635f9eeea9ef555aa65a5b7dd
Zack Garza <dzackgarza@gmail.com>
2016-08-21 17:19:54 -0700

    Save

--- aliases/aliases.zsh
+++ aliases/aliases.zsh
@@ -5,8 +5,8 @@ alias syncjogl_fromdev='rsync -raz zgarza@zgarza.tinyprints.com:/opt/tinyprints/
 
 alias syncjogl_todev='rsync -raz /opt/tinyprints wqzgarza@zgarza.tinyprints.com:/opt/tinyprints/w2p-jogl'
 
-function parHaskell() { 
-	ghc -O2 --make $1 -threaded -rtsopts; 
+function parHaskell() {
+	ghc -O2 --make $1 -threaded -rtsopts;
 }
 
 #export JAVA_HOME='/usr/libexec/java_home'
@@ -34,3 +34,6 @@ alias w2pbuild='cd /opt/tinyprints/web2print_trunk;/dist/apache-tomcat-7.0.29/bi
 
 alias permfix='sudo chown -R zgarza:zgarza /opt/tinyprints && find /opt/tinyprints/w2p-jogl -type f -perm 777 -print -exec chmod 755 {} \;'
 alias crestart='/dist/apache-tomcat-7.0.29/bin/shutdown.sh && /dist/apache-tomcat-7.0.29/bin/startup.sh'
+
+
+alias getqueries="cat ~/.config/calibre/server_access_log.txt| grep -o -E 'query=[^ ]+' | sed 's/query=//' | sed 's/\"//' | sed 's/^[ \t]*//' | sort | uniq"
