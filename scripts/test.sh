ca2bf5587ce7ef447f0db1c78639f4cf8901df22
D. Zack Garza <dzackgarza@gmail.com>
2017-02-21 19:49:36 -0800

    Use absolute paths, fix a few lingering issues.

---
+++ scripts/test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo "The script you are running has basename `basename $0`, dirname `dirname $DIR`"
+echo "The present working directory is `pwd`"
