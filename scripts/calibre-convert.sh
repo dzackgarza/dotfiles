6f0c05801592cc7bc98776ff9ec9e75a057ebaa1
D. Zack Garza <dzackgarza@gmail.com>
2018-06-05 14:25:16 -0700

    Update

--- scripts/calibre-convert.sh
+++ scripts/calibre-convert.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-find . -name "*.epub" -o -name ".djvu" | while read line; do
+find . -name "*.epub" -o -name "*.djvu" | while read line; do
   #base=$(basename "$line")
   new=${line%.*}.pdf
   echo $line
   echo $new
-  /opt/calibre/ebook-convert "$line" "$new"
+  ebook-convert "$line" "$new"
 done
