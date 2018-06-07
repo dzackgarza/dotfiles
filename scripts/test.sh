d4f7e8c07c1739dfd9fae6ac043c6e171a3c3949
Zack Garza <dzackgarza@gmail.com>
2018-06-07 12:57:41 -0700

    Major update

--- scripts/test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-echo "The script you are running has basename `basename $0`, dirname `dirname $DIR`"
-echo "The present working directory is `pwd`"
