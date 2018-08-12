1b778be840ef0bf57e33e2395774894c49d3ced6
zack <dzackgarza@gmail.com>
2018-08-11 23:11:49 -0700

    - ‘bin/stylus_switch’ / ‘bin/stylusfix.sh’

--- bin/stylusfix.sh
+++ bin/stylusfix.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 xinput set-prop "ELAN Touchscreen Pen stylus" --type=float "Coordinate Transformation Matrix" 0.708749 0.000000 0.000000 0.000000 0.500000 0.500000 0.000000 0.000000 \1.000000
+xinput set-prop "ELAN Touchscreen Pen eraser" --type=float "Coordinate Transformation Matrix" 0.708749 0.000000 0.000000 0.000000 0.500000 0.500000 0.000000 0.000000 \1.000000
