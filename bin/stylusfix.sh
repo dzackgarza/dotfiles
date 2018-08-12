711633c81c79c3469259086e09f230c9250add8a
zack <dzackgarza@gmail.com>
2018-08-11 23:11:05 -0700

    / ‘bin/stylusfix.sh’

--- bin/stylusfix.sh
+++ bin/stylusfix.sh
@@ -1,17 +1,2 @@
 #!/bin/bash
-
-devnr=19
-
-# Listen for BTN_TOOL_RUBBER events.
-evtest /dev/input/event$devnr | while read line; do
-for value in {0..1}; do
-    echo $line | grep -q "type 1 (EV_KEY), code 321 (BTN_TOOL_RUBBER), value $value"
-    if [[ $? == 0 ]]; then
-        case $value in
-            0)  xdotool mouseup 3   ;;
-            1)  xdotool mousedown 3 ;;
-        esac
-    fi
-done
-done
 xinput set-prop "ELAN Touchscreen Pen stylus" --type=float "Coordinate Transformation Matrix" 0.708749 0.000000 0.000000 0.000000 0.500000 0.500000 0.000000 0.000000 \1.000000
