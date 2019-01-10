d91ece9646f47f35803c822cb57f54d41af10c2d
zack <dzackgarza@gmail.com>
2019-01-09 22:34:25 -0800

    / ‘bin/stylusfix.sh’

--- bin/stylusfix.sh
+++ bin/stylusfix.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-xsetwacom set "ELAN Touchscreen Pen stylus" MapToOutput eDP1
-xsetwacom set "ELAN Touchscreen Pen eraser" MapToOutput eDP1
+xsetwacom set "ELAN Touchscreen stylus" MapToOutput eDP1
+xsetwacom set "ELAN Touchscreen eraser" MapToOutput eDP1
 
 matrix=$(xinput list-props "ELAN Touchscreen" | grep 'Coordinate Transformation Matrix' | cut -d ':' -f 2 | xargs)
 
-xinput set-prop "ELAN Touchscreen Pen stylus" --type=float "Coordinate Transformation Matrix" $matrix
-xinput set-prop "ELAN Touchscreen Pen eraser" --type=float "Coordinate Transformation Matrix" $matrix
+xinput set-prop "ELAN Touchscreen stylus" --type=float "Coordinate Transformation Matrix" $matrix
+xinput set-prop "ELAN Touchscreen eraser" --type=float "Coordinate Transformation Matrix" $matrix
