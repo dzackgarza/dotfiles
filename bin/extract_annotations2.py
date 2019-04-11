0f24a69e328538dcbd9c8b05d23863386bc76708
zack <dzackgarza@gmail.com>
2019-04-10 20:45:14 -0700

    / ‘bin/extract_annotations2.py’

--- bin/extract_annotations2.py
+++ bin/extract_annotations2.py
@@ -19,31 +19,34 @@ def main():
     bookauthor = doc.info('Author')
     total_annotations = 0
     outstring += f"<h1>{booktitle}, {bookauthor}</h1>\n"
+    popups = []
+    highlights = []
     for i in range(doc.numPages()):
         #print("========= PAGE {} =========".format(i+1))
         page = doc.page(i)
         annotations = page.annotations()
         (pwidth, pheight) = (page.pageSize().width(), page.pageSize().height())
-        if len(annotations) > 0:
-            for annotation in annotations:
-                if  isinstance(annotation, popplerqt5.Poppler.Annotation):
-                    total_annotations += 1
-                    if(isinstance(annotation, popplerqt5.Poppler.HighlightAnnotation)):
-                        quads = annotation.highlightQuads()
-                        txt = ""
-                        for quad in quads:
-                            rect = (quad.points[0].x() * pwidth,
-                                    quad.points[0].y() * pheight,
-                                    quad.points[2].x() * pwidth,
-                                    quad.points[2].y() * pheight)
-                            bdy = PyQt5.QtCore.QRectF()
-                            bdy.setCoords(*rect)
-                            txt = txt + str(page.text(bdy)) + ' '
-
-                        #print(f'========= ANNOTATION, PAGE {i}=========')
-                        fewer_spaces = ' '.join(txt.split()).replace("- ", "")
-                        outstring += f'{fewer_spaces} (<a href="file:///{realpath}#page={i+1}" target="_blank">{bookauthor} {i+1}</a>)</p>\n'
-                        #print("========= END ANNOTATION =========")
+        if len(annotation) < 1:
+            continue
+        for annotation in annotations:
+            if(!isinstance(annotation, popplerqt5.Poppler.Annotation):
+               continue
+            total_annotations += 1
+            txt = ""
+            if(isinstance(annotation, popplerqt5.Poppler.HighlightAnnotation)):
+                quads = annotation.highlightQuads()
+                for quad in quads:
+                    rect = (quad.points[0].x() * pwidth,
+                            quad.points[0].y() * pheight,
+                            quad.points[2].x() * pwidth,
+                            quad.points[2].y() * pheight)
+                    bdy = PyQt5.QtCore.QRectF()
+                    bdy.setCoords(*rect)
+                    txt += str(page.text(bdy)) + ' '
+            if(isinstance(annotation, popplerqt5.Poppler.TextAnnotation)):
+               txt += annotation.contents()
+            fewer_spaces = ' '.join(txt.split()).replace("- ", "")
+            highlights.append(f'{fewer_spaces} (<a href="file:///{realpath}#page={i+1}" target="_blank">{bookauthor} {i+1}</a>)</p>\n')
 
     outstring += "<hr>\n"
     with open(f"/home/zack/Notes/Annotations/{booktitle}.html", "w") as fp:
