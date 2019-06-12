2769def070579a1748c965f419b70c7289838099
D. Zack Garza <dzackgarza@gmail.com>
2019-06-12 16:08:04 -0700

    Update script

--- zsh/aliases/aliases.zsh
+++ zsh/aliases/aliases.zsh
@@ -6,13 +6,6 @@
   #fi
 #}
 
-function jg () {
-  curl -i -H "Accept: application/json" -H "Content-Type: application/json" "$1" -L
-}
-
-function jp () {
-  curl --data "$1" "$2"
-}
 
 function mcd() {
 	mkdir $1 && cd $1
@@ -31,24 +24,15 @@ alias gsave="git add -A && git commit -m 'auto update' && git push origin master
 alias pbcopy='xsel --clipboard --input';
 alias pbpaste='xsel --clipboard --output';
 
-# Directory jumping
-alias ..="cd .."
-alias ...="cd ../.."
-alias ....="cd ../../.."
-alias .....="cd ../../../.."
-
 alias whatsmyip="curl ipinfo.io/ip"
 
 function uninstall() {
   command -v yaourt >/dev/null 2>&1 && sudo yaourt -Rns "$1" --noconfirm;
 }
 
-alias pipi="pip install --user"
-
 function pinstall() {
   command -v yaourt >/dev/null 2>&1 && yaourt -S "$1" --noconfirm;
 }
-
 
 function researchnotes() {
   echo "Enter description for file name";
