a9e33c5782abaf035f455a5fdaf0b48d580b24f9
zack <dzackgarza@gmail.com>
2019-08-15 19:22:04 -0400

    / ‘bin/dumpMacros.js’

--- bin/dumpMacros.js
+++ bin/dumpMacros.js
@@ -1,3 +1,3 @@
 var macs = require("/home/zack/.mume/mathjax_config.js").TeX.Macros
 as  = Object.entries(macs).map(a => `\\newcommand{\\${a[0]}}[${a[1][1]}]{${a[1][0]}}`)
-require('fs').writeFile("/home/zack/Dropbox/Document Archive/Latex/latexmacs.txt", as.join('\n'), a => a)
+require('fs').writeFile("/home/zack/Dropbox/Document Archive/Latex/latexmacs.tex", as.join('\n'), a => a)
