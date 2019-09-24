86066aa122b5b753a0607212496ebbe205d11ffc
zack <dzackgarza@gmail.com>
2019-09-23 22:03:00 -0400

    / ‘bin/dmenuRecentPDFs.sh’

--- bin/dmenuRecentPDFs.sh
+++ bin/dmenuRecentPDFs.sh
@@ -4,15 +4,11 @@ FONT=( 'size=10' 'font=UbuntuMono' )
 
 
 # Recently Opened
-#names=()
-#commands=()
 declare -A BOOKS
 while read THISBOOK; do
   NEWNAME=$(basename $THISBOOK .pdf)
   NEWCMD="${THISBOOK}"
   BOOKS[$NEWNAME]=$NEWCMD
-  #names+=($NEWNAME)
-  #commands+=($NEWCMD)
 done < <(recentlyOpenedPDFs.sh) 
 
 
