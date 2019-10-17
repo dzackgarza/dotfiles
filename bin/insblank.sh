8d3afdcc2942ade130b5e63d6d51850888b57c55
zack <dzackgarza@gmail.com>
2019-10-16 22:45:05 -0400

    + ‘bin/insblank.sh’

---
+++ bin/insblank.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+INPUT_PDF=$1
+BLANK_PDF="/home/zack/dotfiles/blank.pdf"
+NUMPAGES=$(pdftk "${INPUT_PDF}" dump_data 2>/dev/null | grep NumberOfPages  | sed 's/[^0-9]*//')
+PAGES=$(python -c "print ' B1-1 '.join(['A%d-%d'%(x,x) for x in range(1,${NUMPAGES}+1)])")
+
+pdftk A="${INPUT_PDF}" B="${BLANK_PDF}" cat ${PAGES} output "blanked_${INPUT_PDF}"
