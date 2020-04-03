f4ff222c1e4c35173ccc10bc65a329dffd9212fc
zack <dzackgarza@gmail.com>
2020-04-03 17:11:46 -0400

    / ‘bin/monitorHomeLayout.sh’

--- bin/monitorHomeLayout.sh
+++ bin/monitorHomeLayout.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 xrandr --output eDP1 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
 stylusfix
+xset -dpms                                                                                                                                                                                                                     
+xset s noblank
+xset s off
