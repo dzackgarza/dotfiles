ccb0ab23e950a1bd27c678dbba60a5b1aa55d549
D. Zack Garza <dzackgarza@gmail.com>
2020-08-29 04:50:50 +0000

    Save

--- bin/updateAllNotes.sh
+++ bin/updateAllNotes.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/bin/bash
 
 echo "Updating: Algebraic groups."
-cd "/home/zack/SparkleShare/github.com/Notes/Class_Notes/2020/Fall/Algebraic Groups/" && make compile && echo "Made successfully."
+cd "$NOTES/Class_Notes/2020/Fall/Algebraic Groups/" && make compile && echo "Made successfully."
