2897c09f5eee2ecb81c3daf7b190ab6075d34b7b
D. Zack Garza <dzackgarza@gmail.com>
2021-01-03 14:43:27 -0500

    Save

---
+++ bin/notes/newnote.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+now=$(date +%H:%M)
+today=$(date +%Y-%m-%d)
+outfile="$NOTES/Quick_Notes/$today.md"
+
+[[ ! -f $outfile ]] && echo "# $today" > $outfile
+
+nvim -c "norm Go" \
+  -c "norm Go## $now" \
+  -c "norm G2o" \
+  -c "norm zz" \
+  -c "startinsert" $outfile
