468db3541c5163a2fb7bb42638112a3830fc169a
D. Zack Garza <dzackgarza@gmail.com>
2021-08-02 22:22:30 -0400

    Save

--- bin/updates/update_website_and_notes.sh
+++ bin/updates/update_website_and_notes.sh
@@ -1,10 +1,10 @@
 /home/zack/Notes/Obsidian/Unsorted/Quick_Notes/compile_html_notes.sh && echo "Updated math journal.";
 
-cd ~/Quals; git add -A; git commit -am "Save"; git push origin master; popd
+pushd ~/dotfiles; git add -A; git commit -am "Save"; git push origin master; popd
 
-cd ~/Notes && git add *; git commit -am "Save"; git push; popd; ssh zack@dzackgarza.com "cd /var/www/Notes; git stash; git pull;"
-notify-send "Website" "Notes updated." --urgency=critical --expire-time=2000
+pushd ~/Quals; git add -A; git commit -am "Save"; git push origin master; popd
 
-cd ~/website && git add *; git commit -am "Save"; git push; ssh zack@dzackgarza.com ". ~/.rvm/scripts/rvm && ~/updateWebsite.sh"; popd; 
-notify-send "Website" "Website updated." --urgency=critical --expire-time=2000
+pushd ~/Notes && git add *; git commit -am "Save"; git push; popd; ssh zack@dzackgarza.com "cd /var/www/Notes; git stash; git pull;"
+
+pushd ~/website && git add *; git commit -am "Save"; git push; popd; ssh zack@dzackgarza.com ". ~/.rvm/scripts/rvm && ~/updateWebsite.sh"; 
 
