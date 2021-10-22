8aab014260455284ed1611a48c9483425f4efe05
D. Zack Garza <dzackgarza@gmail.com>
2021-10-22 11:24:14 -0400

    Save

--- bin/i3_setup.sh
+++ bin/i3_setup.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 xmodmap -e "clear lock";
- xmodmap -e "keycode 9 = Caps_Lock NoSymbol Caps_Lock";
- xmodmap -e "keycode 66 = Escape NoSymbol Escape";
- xset r rate 280 45;
- xinput set-prop 17 "libinput Natural Scrolling Enabled" 1;
- xinput set-prop 17 "libinput Tapping Enabled" 1;
+xmodmap -e "keycode 9 = Caps_Lock NoSymbol Caps_Lock";
+xmodmap -e "keycode 66 = Escape NoSymbol Escape";
+xset r rate 280 45;
+xinput set-prop 17 "libinput Natural Scrolling Enabled" 1;
+xinput set-prop 17 "libinput Tapping Enabled" 1;
 
 screenchanges.sh;
 nitrogen --restore &
-polybar.sh &
+pkill polybar & polybar.sh > /var/log/polybar.log 2>&1
 pkill redshift; redshift -x; redshift-gtk &
 pkill picom; picom --experimental-backends &
-nm-applet &
-blueman-applet &
+pkill nm-applet & nm-applet &
+pkill blueman-applet & blueman-applet &
+i3l vstack 0.6
