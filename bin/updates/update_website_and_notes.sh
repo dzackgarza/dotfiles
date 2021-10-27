a3fa97a6856ab115c51de1707cea4fe32e7f4c48
D. Zack Garza <dzackgarza@gmail.com>
2021-10-27 15:16:11 -0400

    Save

--- bin/updates/update_website_and_notes.sh
+++ bin/updates/update_website_and_notes.sh
@@ -1,5 +1,3 @@
-/home/zack/Notes/Obsidian/Unsorted/Quick_Notes/compile_html_notes.sh && echo "Updated math journal.";
-
 pushd ~/dotfiles; git add -A; git commit -am "Save"; git push origin master; popd
 
 pushd ~/Quals; git add -A; git commit -am "Save"; git push origin master; popd
