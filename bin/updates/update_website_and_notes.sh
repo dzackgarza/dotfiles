9da62f343caacdcaf3e6e20a3cd17f8ee6bfc58e
D. Zack Garza <dzackgarza@gmail.com>
2022-01-17 23:38:37 -0500

    Save

--- bin/updates/update_website_and_notes.sh
+++ bin/updates/update_website_and_notes.sh
@@ -1,8 +1,14 @@
-pushd ~/dotfiles; git add -A; git commit -am "Save"; git push origin master; popd
-
-pushd ~/Quals; git add -A; git commit -am "Save"; git push origin master; popd
-
-pushd ~/Notes && git add *; git commit -am "Save"; git push; popd; ssh zack@dzackgarza.com "cd /var/www/Notes; git stash; git pull;"
-
 pushd ~/website && git add *; git commit -am "Save"; git push; popd; ssh zack@dzackgarza.com ". ~/.rvm/scripts/rvm && ~/updateWebsite.sh"; 
+pushd ~/Notes && git add *; git commit -am "Save"; git push; popd; ssh zack@dzackgarza.com "cd /var/www/Notes; git stash; git pull;"
+pushd ~/dotfiles; git add -A; git commit -am "Save"; git push origin master; popd;
 
+CurrentNotes=(
+  "/home/zack/SparkleShare/github.com/Notes/Class_Notes/2022/Spring/CommutativeAlgebra"
+  "/home/zack/SparkleShare/github.com/Notes/Class_Notes/2022/Spring/CohomologyRepTheory"
+  "/home/zack/SparkleShare/github.com/Notes/Class_Notes/2022/Spring/FunctionalAnalysis"
+  "/home/zack/SparkleShare/github.com/Notes/Class_Notes/2022/Spring/SheafCohomology"
+)
+
+for val in ${CurrentNotes[@]}; do
+   cd $val; make clean; make all;
+done
