c52eb2aa77f8f5dbcc0ebb0c9b638a77734cf440
D. Zack Garza <dzackgarza@gmail.com>
2022-01-20 14:35:22 -0500

    Save

--- bin/updates/update_website_and_notes.sh
+++ bin/updates/update_website_and_notes.sh
@@ -1,7 +1,3 @@
-pushd ~/website && git add *; git commit -am "Save"; git push; popd; ssh zack@dzackgarza.com ". ~/.rvm/scripts/rvm && ~/updateWebsite.sh"; 
-pushd ~/Notes && git add *; git commit -am "Save"; git push; popd; ssh zack@dzackgarza.com "cd /var/www/Notes; git stash; git pull;"
-pushd ~/dotfiles; git add -A; git commit -am "Save"; git push origin master; popd;
-
 CurrentNotes=(
   "/home/zack/SparkleShare/github.com/Notes/Class_Notes/2022/Spring/CommutativeAlgebra"
   "/home/zack/SparkleShare/github.com/Notes/Class_Notes/2022/Spring/CohomologyRepTheory"
@@ -12,3 +8,8 @@ CurrentNotes=(
 for val in ${CurrentNotes[@]}; do
    cd $val; make clean; make all;
 done
+
+pushd ~/website && git add *; git commit -am "Save"; git push; popd; ssh zack@dzackgarza.com ". ~/.rvm/scripts/rvm && ~/updateWebsite.sh"; 
+pushd ~/Notes && git add *; git commit -am "Save"; git push; popd; ssh zack@dzackgarza.com "cd /var/www/Notes; git stash; git pull;"
+pushd ~/dotfiles; git add -A; git commit -am "Save"; git push origin master; popd;
+
