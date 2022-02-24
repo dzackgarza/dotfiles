581b346774fa9d19a88a5672c848b4dd7e807b89
D. Zack Garza <dzackgarza@gmail.com>
2022-02-23 19:29:47 -0500

    Save

--- bin/updates/update_website_and_notes.sh
+++ bin/updates/update_website_and_notes.sh
@@ -6,9 +6,9 @@ CurrentNotes=(
   "/home/zack/SparkleShare/github.com/Notes/Class_Notes/2022/Spring/ContactTopology"
 )
 
-for val in ${CurrentNotes[@]}; do
-   cd $val; make clean; make all;
-done
+#for val in ${CurrentNotes[@]}; do
+   #cd $val; make clean; make all;
+#done
 
 pushd ~/website && git add *; git commit -am "Save"; git push; popd; ssh zack@dzackgarza.com ". ~/.rvm/scripts/rvm && ~/updateWebsite.sh"; 
 pushd ~/Notes && git add *; git commit -am "Save"; git push; popd; ssh zack@dzackgarza.com "cd /var/www/Notes; git stash; git pull;"
