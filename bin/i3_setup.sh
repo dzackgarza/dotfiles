45f7e921fecee2e657454ec0165c1d6d18934ee8
D. Zack Garza <dzackgarza@gmail.com>
2022-05-24 23:58:30 -0400

    Save

--- bin/i3_setup.sh
+++ bin/i3_setup.sh
@@ -4,10 +4,10 @@ xmodmap -e "clear lock";
 xmodmap -e "keycode 9 = Caps_Lock NoSymbol Caps_Lock";
 xmodmap -e "keycode 66 = Escape NoSymbol Escape";
 xset r rate 280 45;
-xinput set-prop 17 "libinput Natural Scrolling Enabled" 1;
-xinput set-prop 17 "libinput Tapping Enabled" 1;
+xinput set-prop 15 "libinput Natural Scrolling Enabled" 1;
+xinput set-prop 15 "libinput Tapping Enabled" 1;
 
-screenchanges.sh;
+#screenChanges.sh;
 nitrogen --restore &
 pkill polybar & polybar.sh > /var/log/polybar.log 2>&1
 pkill redshift; redshift -x; redshift-gtk &
