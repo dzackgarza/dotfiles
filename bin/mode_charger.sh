5ca5e7728edca51dd5a644fa83ad3eedc5e1b717
D. Zack Garza <dzackgarza@gmail.com>
2023-05-21 00:10:22 +0100

    Save

--- bin/mode_charger.sh
+++ bin/mode_charger.sh
@@ -2,6 +2,6 @@
 
 sparkleshare &
 dropbox &
-echo "power on" | bluetoothctl;
+rfkill unblock all; echo "power on\r\n connect 88:C9:E8:2D:78:82" | bluetoothctl;
 nmcli radio wifi on;
 sudo cpupower frequency-set -u 4.6Ghz;
