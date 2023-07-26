af951b4bb1963db6f8af8c6c770eb2f452e42cdd
D. Zack Garza <dzackgarza@gmail.com>
2023-07-26 10:38:08 +0100

    Save

--- bin/mdimages_to_anki_collection.py
+++ bin/mdimages_to_anki_collection.py
@@ -6,7 +6,6 @@ import subprocess
 
 from mistune.renderers.markdown import MarkdownRenderer
 
-
 # Store the file name in a variable
 file_name = sys.argv[1]
 
@@ -17,8 +16,8 @@ class ImageLinkRenderer(MarkdownRenderer):
         # Add an options attribute to the class
         self.options = {}
 
-    def image(self, src, title, text):
-        self.image_links.append(src)
+    def image(self, token, state):
+        self.image_links.append(token["attrs"]["url"])
         return ""
 
 
@@ -28,23 +27,37 @@ with open(file_name, "r") as f:
   contents = f.read()
 
 # Create an instance of the custom renderer
+print("Opened file, creating image link renderer...")
 renderer = ImageLinkRenderer()
 
+
 # Create a markdown parser using the custom renderer
+print("Creating markdown parser...")
 parser = mistune.Markdown(renderer=renderer)
 
 # Parse the markdown file and extract the image links
+print("Parsing markdown..")
 parser.parse(contents)
 
-# Iterate over the list of image links
+# print("Checking that image_links works:")
+# print(renderer.image_links)
+
+if len(renderer.image_links) == 0:
+    print("No images found in note.")
+    print("*****************")
+    exit()
+
+print("Images found in note, iterating..")
 for link in renderer.image_links:
     if "attachment" in link:
         target_dir="/home/zack/.local/share/Anki2/User 1/collection.media/attachments"
     else:
         target_dir="/home/zack/.local/share/Anki2/User 1/collection.media"
     file_name = link.split("/")[-1]
+    print(file_name)
     # Run the locate command and get the output as a list of lines
     output = subprocess.run(["locate", file_name], capture_output=True).stdout.decode().split("\n")
+    print(output)
 
     # Filter the list to only include files whose directories do not contain "collection" or "trash"
     filtered_output = [line for line in output if "collection" not in line and "trash" not in line and "Trash" not in line]
@@ -59,3 +72,4 @@ for link in renderer.image_links:
         shutil.copy(first_match, target_dir)
     else:
         print("No matches found.")
+print("*****************")
