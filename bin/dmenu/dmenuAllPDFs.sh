ffe46955689a857899998019ccc5e1355d65de6d
Zack Garza <dzackgarza@gmail.com>
2024-05-04 21:19:30 -0400

    Save

--- bin/dmenu/dmenuAllPDFs.sh
+++ bin/dmenu/dmenuAllPDFs.sh
@@ -8,7 +8,7 @@ while read THISBOOK; do
   NEWNAME=$(basename $THISBOOK .pdf)
   NEWCMD="${THISBOOK}"
   BOOKS[$NEWNAME]=$NEWCMD
-done < <(find /home/zack/Dropbox/Library /home/zack/Zotero/storage /home/zack/Downloads type f -iname "*.pdf" -printf '%TY-%Tm-%Td %TT#%p\n' | grep -v Moon | cut -d "#" -f 2) 
+done < <(find /home/dzack/Dropbox/Library /home/dzack/Zotero/storage /home/dzack/Downloads -type f -iname "*.pdf" -printf '%TY-%Tm-%Td %TT#%p\n' | grep -v Moon | cut -d "#" -f 2) 
 
-choice=$(printf "%s\n" "${!BOOKS[@]}" | dmenu -i )
+choice=$(printf "%s\n" "${!BOOKS[@]}" | dmenu -i -l 20)
 [ -z "$choice" ] || nohup okular "${BOOKS[$choice]}" & disown
