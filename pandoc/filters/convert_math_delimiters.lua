39d4383363ba10e4074ee1e2d09465f0dd4577d6
D. Zack Garza <dzackgarza@gmail.com>
2024-05-05 22:43:40 -0400

    Save

---
+++ pandoc/filters/convert_math_delimiters.lua
@@ -0,0 +1,46 @@
+-- Specify delimiters for each type of output
+function Math(m)
+  -- For markdown->markdown cleaning
+  if FORMAT:match "markdown" then
+    if m.mathtype == "InlineMath" then
+      return pandoc.RawInline('markdown', '\\( ' .. m.text .. ' \\)')
+    else
+      return pandoc.RawInline('markdown', '\n\\[' .. m.text .. '\\]\n')
+    end
+  end 
+
+  -- For Latex and HTML output
+  if FORMAT:match "latex" or FORMAT:match "pdf" then
+    if m.mathtype == "InlineMath" then
+      return m
+      --return pandoc.RawInline('tex', '\\(' .. m.text .. '\\)')
+    else
+      if string.find(m.text, "label") then
+        return pandoc.RawInline('tex', '\n\\begin{equation}'.. m.text .. '\\end{equation}\n')
+      else
+        return pandoc.RawInline('tex', '\n\\begin{align*}'.. m.text .. '\\end{align*}\n')
+      end
+    end
+  end 
+
+  if FORMAT:match "html" or FORMAT:match "html5" then
+    if m.mathtype == "InlineMath" then
+      return m
+    else
+      return pandoc.RawInline('html', '\n<span class="math display">\n\\begin{align*}'.. m.text .. '\\end{align*}\n<span>')
+    end
+  end 
+
+end
+
+-- Delete macro inclusions 
+function RawBlock(el)
+  if FORMAT:match "markdown" then
+    if string.find(el.text, "newcommand") or string.find(el.text, "DeclareMath") then
+      --print("Found newcommand")
+      return pandoc.Para("")
+    else
+      return el
+    end
+  end
+end
