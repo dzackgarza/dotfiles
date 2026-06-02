ec784f95063310412b0cdb8e97c4424a0abad94d
Zack Garza <dzackgarza@gmail.com>
2026-06-03 07:37:02 +0800

    Automatic save.

--- pandoc/filters/components.lua
+++ pandoc/filters/components.lua
@@ -309,7 +309,9 @@ function Div(el)
     fail(".component div missing type attribute")
   end
 
-  if component_type == "teaching-timeline" then
+  if component_type == "tweet" then
+    return render_tweet(el)
+  elseif component_type == "teaching-timeline" then
     return render_teaching_timeline(el)
   elseif component_type == "collapse" then
     return render_collapse(el)
